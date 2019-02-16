a85a7f131c82521728ab6380c2a55bb22345eeeb
Tom van den Broecke <tomvdbroecke@outlook.com>
2019-02-16 12:53:36 +0100

    Changed Favicon's and added OGP integration

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,30 +14,19 @@
 
         <title>Tom van den Broecke</title>
 
+        <!-- OGP Tags -->
+        <meta property="og:title" content="Tom van den Broecke">
+        <meta property="og:description" content="This is a portfolio website by Tom van den Broecke.">
+        <meta property="og:image" content="{{ URL::asset('assets/thumbnail.png') }}">
+        <meta property="og:url" content="https://www.tomvdbroecke.com">
+        <meta property="og:type" content="website">
+
         <!-- Metatags -->
         <meta name="author" content="Tom van den Broecke">
         <meta name="description" content="This is a portfolio website by Tom van den Broecke.">
         <meta name="keywords" content="HTML, CSS, JavaScript, PHP, C#, Programming, Development, Developing, Design, Websites, Full-Stack">
+        <meta name="image" content="{{ URL::asset('assets/thumbnail.png') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
-
-        <!-- Style Includes -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <link rel="stylesheet" href="{{ URL::asset('css/home_style.css') }}">
-
-        <!-- Include Favicons -->
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('assets/favicons/apple-touch-icon.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ URL::asset('assets/favicons/favicon-32x32.png') }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ URL::asset('assets/favicons/favicon-16x16.png') }}">
-        <link rel="icon" type="image/png" sizes="192x192" href="{{ URL::asset('assets/favicons/favicon-192x192.png') }}">
-        <link rel="icon" type="image/png" sizes="264x168" href="{{ URL::asset('assets/favicons/speeddial-264x168.png') }}">
-        <link rel="manifest" href="{{ URL::asset('assets/favicons/site.webmanifest') }}">
-        <link rel="mask-icon" href="{{ URL::asset('assets/favicons/safari-pinned-tab.svg') }}" color="#5bbad5">
-        <link rel="shortcut icon" href="{{ URL::asset('assets/favicons/favicon.ico') }}">
-        <meta name="msapplication-TileColor" content="#2b5797">
-        <meta name="msapplication-config" content="{{ URL::asset('assets/favicons/browserconfig.xml') }}">
-        <meta name="theme-color" content="#060d21">
 
         <!-- Structured Data -->
         <script type="application/ld+json">
@@ -54,6 +43,25 @@
             }]
         }
         </script>
+
+        <!-- Style Includes -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <link rel="stylesheet" href="{{ URL::asset('css/home_style.css') }}">
+
+        <!-- Include Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('assets/favicons/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ URL::asset('assets/favicons/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ URL::asset('assets/favicons/favicon-16x16.png') }}">
+        <link rel="icon" type="image/png" sizes="256256" href="{{ URL::asset('assets/favicons/favicon-256x256.png') }}">
+        <link rel="icon" type="image/png" sizes="264x168" href="{{ URL::asset('assets/favicons/speeddial-264x168.png') }}">
+        <link rel="manifest" href="{{ URL::asset('assets/favicons/site.webmanifest') }}">
+        <link rel="mask-icon" href="{{ URL::asset('assets/favicons/safari-pinned-tab.svg') }}" color="#5bbad5">
+        <link rel="shortcut icon" href="{{ URL::asset('assets/favicons/favicon.ico') }}">
+        <meta name="msapplication-TileColor" content="#2b5797">
+        <meta name="msapplication-config" content="{{ URL::asset('assets/favicons/browserconfig.xml') }}">
+        <meta name="theme-color" content="#060d21">
     </head>
     <body>
         <!-- Initialize Scroll Animations -->
